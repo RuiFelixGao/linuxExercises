e82b57bd82747f05ce61ffd61c4fefeacb6b9ef3
kunyi <khan223@wisc.edu>
2025-10-16 13:55:16 -0500

    Finish school.sh

--- school.sh
+++ school.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+# school.sh - find average TotalAssessedValue for "MADISON SCHOOLS" district
+
+cat Property_Tax_Roll.csv | \
+grep "MADISON SCHOOLS" | \
+cut -d',' -f7 | \
+{
+  sum=0
+  count=0
+  while read value; do
+    if [[ "$value" =~ ^[0-9]+$ ]]; then
+      sum=$((sum + value))
+      count=$((count + 1))
+    fi
+  done
+  echo "Average TotalAssessedValue for MADISON SCHOOLS: $((sum / count))"
+}
